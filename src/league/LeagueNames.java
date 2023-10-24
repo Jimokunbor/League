917be8281b73abb940b32c26cc44a2ae1e8c9141
Jimokunbor <2022473@student.cct.ie>
2023-10-24 18:27:28 +0100

    submission
    Add public Git link as comment to NetBeans program.

--- src/league/LeagueNames.java
+++ src/league/LeagueNames.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ * https://github.com/Jimokunbor/League
+ * 
  */
 package league;
 
